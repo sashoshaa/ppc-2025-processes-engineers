4028ddf0489b0fbc15976547691745f502880e74
sashoshaa <a.soosnina@naash.ru>
2025-11-07 22:52:06 +0300

    fix mpi.cpp

--- tasks/sosnina_a_diff_count/mpi/src/ops_mpi.cpp
+++ tasks/sosnina_a_diff_count/mpi/src/ops_mpi.cpp
@@ -58,9 +58,9 @@ bool SosninaADiffCountMPI::RunImpl() {
   std::size_t remainder = total_len % size;
 
   std::size_t start =
-  (static_cast<std::size_t>(rank) * block_size) + std::min(static_cast<std::size_t>(rank), remainder);
+      (static_cast<std::size_t>(rank) * block_size) + std::min(static_cast<std::size_t>(rank), remainder);
   std::size_t end = start + block_size;
-  if (std::cmp_less(rank, remainder)) { 
+  if (std::cmp_less(rank, remainder)) {
     end += 1;
   }
   end = std::min(end, total_len);
