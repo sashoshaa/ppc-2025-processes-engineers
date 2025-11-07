6d6dc4d718b9adc6b6250a528cec2fca10576b93
sashoshaa <a.soosnina@naash.ru>
2025-11-07 17:03:48 +0300

    Fix undefined functions and code formatting

--- tasks/sosnina_a_diff_count/mpi/include/ops_mpi.hpp
+++ tasks/sosnina_a_diff_count/mpi/include/ops_mpi.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <string>
 #include <cstddef>
-#include <cstdint> 
+#include <cstdint>
+#include <string>
 
 #include "sosnina_a_diff_count/common/include/common.hpp"
 #include "task/include/task.hpp"
--- tasks/sosnina_a_diff_count/mpi/src/ops_mpi.cpp
+++ tasks/sosnina_a_diff_count/mpi/src/ops_mpi.cpp
@@ -56,10 +56,11 @@ bool SosninaADiffCountMPI::RunImpl() {
   std::size_t block_size = total_len / size;
   std::size_t remainder = total_len % size;
 
-  std::size_t start = (static_cast<std::size_t>(rank) * block_size) + std::min(static_cast<std::size_t>(rank), remainder);
+  std::size_t start =
+      (static_cast<std::size_t>(rank) * block_size) + std::min(static_cast<std::size_t>(rank), remainder);
   std::size_t end = start + block_size;
   if (static_cast<std::size_t>(rank) < remainder) {
-      end += 1;
+    end += 1;
   }
   end = std::min(end, total_len);
 
