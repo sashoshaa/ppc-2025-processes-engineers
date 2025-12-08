ad6f60be6e1cd6ccdb7313d5047639e52cb9b048
sashoshaa <a.soosnina@naash.ru>
2025-12-08 21:22:26 +0300

    120 push

--- tasks/sosnina_a_matrix_mult_horizontal/seq/src/ops_seq.cpp
+++ tasks/sosnina_a_matrix_mult_horizontal/seq/src/ops_seq.cpp
@@ -9,7 +9,6 @@ namespace sosnina_a_matrix_mult_horizontal {
 
 SosninaAMatrixMultHorizontalSEQ::SosninaAMatrixMultHorizontalSEQ(InTypeTriple in) : input_(std::move(in)) {
   SetTypeOfTask(GetStaticTypeOfTask());
-  // Инициализация вывода как пустой матрицы
   GetOutput() = std::vector<std::vector<double>>();
 }
 
@@ -17,12 +16,10 @@ bool SosninaAMatrixMultHorizontalSEQ::ValidationImpl() {
   const auto &matrixA = input_.first;
   const auto &matrixB = input_.second;
 
-  // Проверка на пустые матрицы
   if (matrixA.empty() || matrixB.empty()) {
     return false;
   }
 
-  // Проверка, что все строки матрицы A имеют одинаковый размер
   size_t colsA = matrixA[0].size();
   for (size_t i = 1; i < matrixA.size(); i++) {
     if (matrixA[i].size() != colsA) {
@@ -30,13 +27,13 @@ bool SosninaAMatrixMultHorizontalSEQ::ValidationImpl() {
     }
   }
 
-  // Проверка, что все строки матрицы B имеют одинаковый размер
   size_t colsB = matrixB[0].size();
   for (size_t i = 1; i < matrixB.size(); i++) {
     if (matrixB[i].size() != colsB) {
       return false;
     }
   }
+
   return colsA == matrixB.size();
 }
 
@@ -51,23 +48,17 @@ bool SosninaAMatrixMultHorizontalSEQ::RunImpl() {
 
   size_t rowsA = matrixA.size();
   size_t colsA = matrixA[0].size();
-  size_t rowsB = matrixB.size();
   size_t colsB = matrixB[0].size();
 
   auto &output = GetOutput();
-
   output = std::vector<std::vector<double>>(rowsA, std::vector<double>(colsB, 0.0));
 
+  // Умножение матриц
   for (size_t i = 0; i < rowsA; i++) {
-    const auto &rowA = matrixA[i];
-    auto &rowOutput = output[i];
-
     for (size_t k = 0; k < colsA; k++) {
-      double aik = rowA[k];
-      const auto &rowB = matrixB[k];
-
+      double aik = matrixA[i][k];
       for (size_t j = 0; j < colsB; j++) {
-        rowOutput[j] += aik * rowB[j];
+        output[i][j] += aik * matrixB[k][j];
       }
     }
   }
